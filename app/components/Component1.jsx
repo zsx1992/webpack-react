535e7409809df035ec3d4dee9a241e1ff108826e
zsx <lizl@umiit.cn>
2018-05-23 18:00:01 +0800

    react+webpack项目搭建

---
+++ app/components/Component1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+class Component1 extends React.Component{
+    render(){
+        return (
+            <div> 德玛西亚 兽人永不为奴 哈哈 你好</div>
+        )
+    }
+}
+
+export default Component1;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+module.exports = {
+    devtool: 'eval-source-map',//生成Source Maps,这里选择eval-source-map
+    entry: ['webpack/hot/dev-server',__dirname + "/app/main.js"],//唯一入口文件
+    output: {//输出目录
+        path: __dirname + "/build",//打包后的js文件存放的地方
+        filename: "bundle.js"//打包后输出的js的文件名
+    },
+
+    module: {
+        //loaders加载器
+        rules: [
+            {
+                test: /\.(js|jsx)$/,//一个匹配loaders所处理的文件的拓展名的正则表达式，这里用来匹配js和jsx文件（必须）
+                exclude: /node_modules/,//屏蔽不需要处理的文件（文件夹）（可选）
+                use:{
+                    loader: 'babel-loader'//loader的名称（必须）
+                }
+            }
+        ]
+    },
+    devServer:{
+        contentBase: './build',//默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录（本例设置到"build"目录）
+        historyApiFallback: true,//在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+        inline: true,//设置为true，当源文件改变时会自动刷新页面
+        port: 8080,//设置默认监听端口，如果省略，默认为"8080"
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()//热模块替换插件
+    ],
+
+};
+
